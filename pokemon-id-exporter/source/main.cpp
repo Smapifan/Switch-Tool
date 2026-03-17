099d59b65531be26659f94aedd83338519cbbc4f
Smapifan <luca.vogel@gski.de>
2026-03-17 15:16:10 +0100

    Clean up main.cpp by removing commented code
    Removed commented-out code for example data and export fallback.

--- pokemon-id-exporter/source/main.cpp
+++ pokemon-id-exporter/source/main.cpp
@@ -18,7 +18,6 @@ struct PokemonInfo {
     std::map<std::string, std::string> translations;
 };
 
-// Beispiel-Daten (beliebig ausbaubar)
 std::map<std::string, std::vector<ItemInfo>> gameItems = {
     {"firered",   { {1,"POKE BALL", {{"en","POKE BALL"},{"de","POKEBALL"}}}, {2,"POTION", {{"en","POTION"}}} }},
     {"leafgreen", { {1,"POKE BALL", {{"en","POKE BALL"},{"de","POKEBALL"},{"fr","BALLE"}}}, {2,"POTION", {{"en","POTION"}}} }},
@@ -32,7 +31,6 @@ std::map<std::string, std::vector<PokemonInfo>> gamePokemon = {
     {"emerald", { {3,"Treecko", {{"de","Geckarbor"}}} }}
 };
 
-// mkdirp für sdmc:/...
 static void mkdirp(const std::string& path) {
     for (size_t i = 1; i <= path.size(); ++i)
         if (i == path.size() || path[i] == '/')
@@ -47,7 +45,6 @@ int main(int argc, char* argv[]) {
     for (const auto& game : gameItems) {
         std::string gameName = game.first;
         for (const auto& item : game.second) {
-            // Export für alle übersetzten Namen (Sprachen)
             for (const auto& trans : item.translations) {
                 std::string lang = trans.first;
                 std::string fname = gameName;
@@ -59,7 +56,6 @@ int main(int argc, char* argv[]) {
                     fclose(outf);
                 }
             }
-            // Englisch-Fallback falls nicht im translations
             if (item.translations.count("en") == 0) {
                 std::string idFile = "sdmc:/switch/PKMswitch/assets/IDs/" + gameName + "_items.txt";
                 FILE* outf = fopen(idFile.c_str(), "a");
@@ -75,7 +71,6 @@ int main(int argc, char* argv[]) {
     for (const auto& game : gamePokemon) {
         std::string gameName = game.first;
         for (const auto& poke : game.second) {
-            // Export für alle übersetzten Namen (Sprachen)
             for (const auto& trans : poke.translations) {
                 std::string lang = trans.first;
                 std::string fname = gameName;
@@ -87,7 +82,6 @@ int main(int argc, char* argv[]) {
                     fclose(outf);
                 }
             }
-            // Englisch-Fallback falls nicht im translations
             if (poke.translations.count("en") == 0) {
                 std::string idFile = "sdmc:/switch/PKMswitch/assets/IDs/" + gameName + "_pokemon.txt";
                 FILE* outf = fopen(idFile.c_str(), "a");

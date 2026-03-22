8e942df26bf2df48a12e6e5b7ca20e1817f00196
Smapifan <luca.vogel@gski.de>
2026-03-22 12:40:37 +0100

    Delete source/ui/screen_plugin_error.hpp

--- source/ui/screen_plugin_error.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../app_state.hpp"
-
-namespace UI {
-/// Render the "plugin missing / invalid" error screen.
-/// Sets state.shouldExit = true when the user chooses to exit.
-void renderPluginError(AppState& state);
-} // namespace UI
